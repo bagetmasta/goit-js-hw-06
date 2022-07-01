caf42845840ce1e1e14fbafef6defd145371ecd8
Artem Bahmet <baget.masta@gmail.com>
2022-07-01 16:26:07 +0300

    Добавляет начальные файлы

---
+++ src/js/task-01.js
@@ -0,0 +1,10 @@
+const itemEl = document.querySelector('.item h2');
+// console.log(itemEl);
+
+const testText = itemEl.textContent;
+// console.log(testText);
+
+// console.log(itemEl.parentNode);
+
+const titleEl = document.querySelector('h2');
+console.log(titleEl.parentNode);
